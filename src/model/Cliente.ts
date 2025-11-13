7eae1f09c856a54b293ba514deeeef773c083824
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2025-11-13 15:17:26 -0300

    Ajuste nos nomes das variaveis e adição de condições em produto

--- src/model/Cliente.ts
+++ src/model/Cliente.ts
@@ -1,27 +1,49 @@
 import { Produto } from "./Produto";
 
 export class Cliente extends Produto {
-  private nomeCliente: string;
-  private numeroCliente: number;
+  private _nomeCliente: string;
+  private _numeroCliente: number;
+  nome: any;
 
   constructor(
-    idCliente: number,
-    nomeProduto: string,
+    id: number,
+    nome: string,
     preco: number,
     estoque: number,
     nomeCliente: string,
     numeroCliente: number
   ) {
-    super(idCliente, nomeProduto, preco, estoque);
-    this.nomeCliente = nomeCliente;
-    this.numeroCliente = numeroCliente;
+    super(id, nome, preco, estoque);
+    this._nomeCliente = nomeCliente;
+    this._numeroCliente = numeroCliente;
+  }
+
+  public get nomeCliente(): string {
+    return this._nomeCliente;
+  }
+
+  public set nomeCliente(valor: string) {
+    this._nomeCliente = valor;
+  }
+
+  public get numeroCliente(): number {
+    return this._numeroCliente;
+  }
+
+  public set numeroCliente(valor: number) {
+    this._numeroCliente = valor;
   }
 
   public visualizar(): void {
-    console.log("\n=== Detalhes da Compra ===");
-    console.log(`Cliente: ${this.nomeCliente} (Nº ${this.numeroCliente})`);
-    console.log(`Produto: ${this.getNomeProduto()}`);
-    console.log(`Preço: R$ ${this.getPreco().toFixed(2)}`);
-    console.log(`Estoque restante: ${this.getEstoque()}`);
+    console.log(`
+****************************************************
+🧾 DADOS DA COMPRA
+****************************************************
+Cliente: ${this._nomeCliente} (Nº ${this._numeroCliente})
+Produto: ${this.nome}
+Preço: R$ ${this.preco.toFixed(2)}
+Estoque disponível: ${this.estoque}
+****************************************************
+`);
   }
 }
